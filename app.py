14afff92636e77510064b969ed6dfdbffe581223
gartician <kongga2017@gmail.com>
2020-06-04 13:10:31 -0700

    mesh plot added. table styled and rounding fixed with Format(). README button added but not linked (ideally to Confluence page).

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ import os
 import sys
 import re
 import dash
+import itertools
 import dash_table
+import pandas as pd
+import plotly.graph_objs as go
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
-import plotly.graph_objs as go
-import itertools
-import pandas as pd
+from dash.dependencies import Input, Output
+from dash_table.Format import Format, Scheme, Sign, Symbol
 
 # Initiate the app ----------------------------------------------------------------
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -95,9 +96,15 @@ sidebar = html.Div(
             html.Hr(),
             dbc.Label("View effect of frequency and time"),
             dbc.Button(
-                "View",
+                "View (wait 8 seconds)",
                 id="input_view",
-                color = "primary")
+                color = "primary"),
+            html.Hr(),
+            dbc.Button(
+                "README",
+                id="link_readme",
+                color="primary",
+                href="https://www.google.com")
             ],
             vertical=True,
             pills=True,
@@ -108,11 +115,11 @@ sidebar = html.Div(
 
 content = html.Div(id="page-content", style=CONTENT_STYLE)
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
-
-
 # app layout ----------------------------------------------------------------------
 app.layout = html.Div([
+    # app pages
+    dcc.Location(id='url', refresh=False),
+    dcc.Link('Navigate to home', href='/'),
     # title
     dbc.Row(
         dbc.Col(
@@ -133,50 +140,44 @@ app.layout = html.Div([
     # graph cost over time
     dbc.Row(
         dbc.Col(
+            # dcc.Loading(id = 'loading_graph',children = [html.Div(dcc.Graph(id='output_graph'))], type = "default"),
             dcc.Graph(id='output_graph'),
-            width={"size": 7, "offset": 3}
+            width={"offset": 3}
     )),
 
     # table of cost over time
     dbc.Row(
         dbc.Col(
-            # html.H1("hello world", style={"white-space": "nowrap"}), 
-            dash_table.DataTable(id='output_table'),
-            width={"size": 7, "offset": 3}
+            dash_table.DataTable(
+                id='output_table', 
+                style_table = {"height": 500, "overflowY": "auto", "width": "100%"}, 
+                style_as_list_view = True,
+                style_header = {'backgroundColor': 'white', 'fontWeight': 'bold'},
+                style_cell_conditional = [
+                    {'if': {'column_id': 'Cost'}, 'width': 333},
+                    {'if': {'column_id': 'Frequency'}, 'width': 333},
+                    {'if': {'column_id': 'Number of Days'}, 'width': 333}]),
+            width={"size": 6, "offset": 4}
         ))
     ]
 )
 
 # tips:
 # dbc.Alert() children is an empty list right now. Once it receives all the information then the callback will add the price to the dbc.Alert children.
+# style_table height and width should be 100% to take over the size of the allocated grid. If the table is tiny, then you should adjust column width.
+# however if you want overflow Y (vertical scrollbar), then you have to have a defined height.
+# set a graph into a dcc.Loading children to get a loading screen.
 
 # vars and f(x) -------------------------------------------------------------------
 su_dollar = 0.025 # number of dollars per service unit
-# note: total gpu units in a node is hard-coded as 4 below.
+# note: total gpu units in a node is hard-coded as 4 below. Introduce a tot_gpu option if hardware changes in the future.
 
 # define top resource used
 def top_resource(alloc_CPU, cpu_denominator, alloc_GPU, gpu_denominator, alloc_RAM, ram_denominator):
     return( max([alloc_CPU / cpu_denominator, alloc_GPU / gpu_denominator, alloc_RAM / ram_denominator]) )
 
-def cost_graph(table):
-    fig = go.Figure(data=[go.Mesh3d(z=table['Cost'], 
-                                x=table['Number of Days'], 
-                                y = table['Frequency'], 
-                                opacity=1, 
-                                intensity=table['Cost'], 
-                                colorscale="Inferno")])
-    fig.update_layout(
-        title="Job cost over time and frequency",
-        width=700, height=700,
-        scene = dict(
-        xaxis_title="Number of Days (X)",
-        yaxis_title="Frequency (Y)",
-        zaxis_title="Cost (Z)"
-    ))
-
-# plot_mesh3d(wat)
-
-def cost_table(est_cost, max_days = 31, max_freq = 100, units = "units_su"):
+# calculate cost of job over time and frequency
+def cost_table(est_cost, max_days = 32, max_freq = 101, units = "units_su"):
     """ 
     Input: cost of job
     Output: a plot of job cost over time (x) and frequency (y)
@@ -190,8 +191,7 @@ def cost_table(est_cost, max_days = 31, max_freq = 100, units = "units_su"):
     elif units == "units_dollars":
         df_surface = df_surface.assign(Cost = df_surface['Number of Days'] * df_surface['Frequency'] * est_cost * su_dollar)
     else:
-        print("incorrect unit type. Must be 'su' or 'dollars'. ")
-    # surface_plot = plot_mesh3d(df_surface)
+        print("incorrect unit type. Must be 'units_su' or 'units_dollars'. ")
     return(df_surface)
 
 # Service Units Equation = SUM over allocated nodes(max(AllocCPU/TotCPU, AllocRAM/TotRAM, AllocGRES/TotGRES) * NTF) * 28 Service Units/hour * job duration in hours
@@ -207,7 +207,8 @@ def cost_table(est_cost, max_days = 31, max_freq = 100, units = "units_su"):
     [Output("output_su", 'children'),
     Output("output_su", "is_open"),
     Output("output_table", "data"),
-    Output("output_table", "columns")],
+    Output("output_table", "columns"),
+    Output("output_graph", "figure")],
     [Input('node_type', 'value'),
     Input('node_count', 'value'),
     Input('input_cpu', 'value'),
@@ -222,7 +223,8 @@ def calc_cost(node_type, node_count, cpu, gpu, ram, duration, units, n_click):
     if node_type == None:
         table_data = [] # empty table
         table_columns = []
-        return(None, False, table_data, table_columns)
+        fig = go.Figure(data=[go.Mesh3d(x=[],y=[],z=[])])
+        return(None, False, table_data, table_columns, fig)
         pass
     # adjust NTF, total RAM, total CPU by node type
     if node_type == 'std':
@@ -246,18 +248,35 @@ def calc_cost(node_type, node_count, cpu, gpu, ram, duration, units, n_click):
     su = ( (node_count * (max_resource * node_factor)) * 28 * duration )
     # adjust output msg by units selected
     if units == "units_su":
-        est_cost  = "estimated service units: {}".format(su)
+        est_cost  = "estimated service units: {}".format(round(su, 2))
     if units == "units_dollars":
-        est_cost = "estimated cost in dollars: {}".format(su * su_dollar)
-    # plot the table upon button click
+        est_cost = "estimated cost in dollars: ${}".format(round(su * su_dollar, 2))
+    # plot the table upon odd button click (1, 3, 5, ...)
     if n_click == None or n_click % 2 == 0:
         table_data = [] # empty table
         table_columns = []
+        fig = go.Figure(data=[go.Mesh3d(x=[],y=[],z=[])])
     elif (n_click % 2 == 1):
+        # build the table. data and styling goes here! Format() is a lifesaver.
         tbl = cost_table(su, units = units)
         table_data = tbl.to_dict('records')
-        table_columns = [{"name": i, "id": i} for i in tbl.columns]
-    return(est_cost, True, table_data, table_columns)
+        table_columns = [{"name": i, "id": i, "type": "numeric", "format": Format(precision=4)} for i in tbl.columns]
+        # build the graph. data and styling goes here!
+        fig = go.Figure(
+            data=[go.Mesh3d(z=tbl['Cost'], 
+            x=tbl['Number of Days'], 
+            y = tbl['Frequency'], 
+            opacity=1, 
+            intensity=tbl['Cost'], 
+            colorscale="Inferno")])
+        fig.update_layout(
+            title="Job cost over time and frequency",
+            scene = dict(
+            xaxis_title="Number of Days (X)",
+            yaxis_title="Frequency (Y)",
+            zaxis_title="Cost (Z)"),
+            width=1000, height=800)
+    return(est_cost, True, table_data, table_columns, fig)
 
 # if you want an empty table, just return an empty list [] for output table's (data and columns) component id.
 
@@ -266,22 +285,22 @@ def calc_cost(node_type, node_count, cpu, gpu, ram, duration, units, n_click):
 # unit test incompatible with public deployment, but still passes for personal deploy to browser.
 
 # Example 1 (CPU driven SU): User A submits a job that is allocated 14 cores and 32 GB of RAM on one standard compute node.  Each compute node has a total of 28 cores and 128GB of RAM.  The job runs for 10 hours.  The job would have consumed
-# assert float( re.findall("[\d.]+", calc_cost('std', 1, 14, 0, 32, 10, "units_su") )[0]) == 140.0, "1 standard node using 14 cores and 28 GB RAM for 10 hrs does not equal 140 service units"
+assert float( re.findall("[\d.]+", calc_cost('std', 1, 14, 0, 32, 10, "units_su", 0) )[0]) == 140.0, "1 standard node using 14 cores and 28 GB RAM for 10 hrs does not equal 140 service units"
 
 # Example 2 (Memory driven SU): User B submits a job that is allocated 7 cores and 128GB of RAM and one GPU on a GPU node. Each GPU node has a total of 28 cores and 256GB of RAM and 4 GPUs.  The job runs for 10 hours. Then the job would have consumed
-# assert float( re.findall("[\d.]+", calc_cost('std', 1, 7, 1, 128, 10, "units_su") )[0]) == 280.0, "1 standard node using 7 cores and 128 GB RAM for 10 hrs does not equal 280 service units" # accommodate one gpu core
+assert float( re.findall("[\d.]+", calc_cost('std', 1, 7, 1, 128, 10, "units_su", 0) )[0]) == 280.0, "1 standard node using 7 cores and 128 GB RAM for 10 hrs does not equal 280 service units" # accommodate one gpu core
 
 # Example 3 (GPU driven SU): User C submits a job to the GPU partition and that job is allocated 1 core, 16GB of RAM, and 3 GPUs. The nodes in the GPU partition have 28 cpus, 256 GB of RAM, and 4 GPUs. This job runs for 10 hours and will have consumed
-# assert float( re.findall("[\d.]+", calc_cost('gpu', 1, 1, 3, 16, 10, "units_su") )[0]) == 420.0, "1 standard node using 7 cores and 128 GB RAM for 10 hrs does not equal 280 service units" # accommodate one gpu core
+assert float( re.findall("[\d.]+", calc_cost('gpu', 1, 1, 3, 16, 10, "units_su", 0) )[0]) == 420.0, "1 standard node using 7 cores and 128 GB RAM for 10 hrs does not equal 280 service units" # accommodate one gpu core
 
 # Example 4 (CPU driven SU on Fat nodes): User D submits a job to the fat partition that is allocated 42 of the 56 available cpus and 512GB of memory.  The job finishes in 10 hours and will have consumed
-# assert float( re.findall("[\d.]+", calc_cost('fat', 1, 42, 0, 512, 10, "units_su") )[0]) == 1260, "1 fat node using 42 cores and 512 GB RAM for 10 hrs does not equal 1260 service units"
+assert float( re.findall("[\d.]+", calc_cost('fat', 1, 42, 0, 512, 10, "units_su", 0) )[0]) == 1260, "1 fat node using 42 cores and 512 GB RAM for 10 hrs does not equal 1260 service units"
 
 # Example 5 (Memory driven SU on Fat nodes): User E submits a job to the fat partition that is allocated 4 of the 56 available cpus and 2TB (2048GB) of memory.  The job finishes in 10 hours and will have consumed
-# assert float( re.findall("[\d.]+", calc_cost('fat', 1, 4, 0, 2048, 10, "units_su") )[0]) == 3360.0, "1 fat node using 4 cores and 2048 GB RAM for 10 hrs does not equal 3360 service units"
+assert float( re.findall("[\d.]+", calc_cost('fat', 1, 4, 0, 2048, 10, "units_su", 0) )[0]) == 3360.0, "1 fat node using 4 cores and 2048 GB RAM for 10 hrs does not equal 3360 service units"
 
 # Example 6 (Multiple standard nodes): User F submits a job that is allocated 16 standard nodes (28 cores and 128 GB of RAM per node, totaling 448 cores and 2048GB of memory).  The job runs for 10 hours and will have consumed
-# assert float( re.findall("[\d.]+", calc_cost('std', 16, 28, 0, 128, 10, "units_su") )[0]) == 4480.0, "16 std nodes using 28 cores and 128 GB RAM for 10 hrs does not equal 4480 service units"
+assert float( re.findall("[\d.]+", calc_cost('std', 16, 28, 0, 128, 10, "units_su", 0) )[0]) == 4480.0, "16 std nodes using 28 cores and 128 GB RAM for 10 hrs does not equal 4480 service units"
 
 # run app --------------------------------------------------------------------------
 
@@ -307,3 +326,8 @@ if __name__ == '__main__':
 # how do i include a good looking navbar with bootstrap?
 # how do i incorporate 2 sliders into a graph?
 # how do i make multi-page app?
+# how do i set column width to a percentage? 
+
+# dash resources --------------------------------------------------------------------
+
+# dash datatable width, height, styling: https://dash.plotly.com/datatable/width https://dash.plotly.com/datatable/style
