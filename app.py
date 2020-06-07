4d4b26d41641af6706065692287d3702fe2fef74
gartician <kongga2017@gmail.com>
2020-06-06 21:55:01 -0700

    removed dash daq package

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import dash
 import itertools
 import dash_table
 import pandas as pd
-import dash_daq as daq
 import plotly.graph_objs as go
 import dash_core_components as dcc
 import dash_html_components as html
